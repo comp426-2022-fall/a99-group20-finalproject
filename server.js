974085e6524641abf340e1b338fdf32136c10043
Andrew Best <acandrew@email.unc.edu>
2022-12-06 22:45:18 -0500

    created server.js, have root path

---
+++ server.js
@@ -0,0 +1,16 @@
+#!usr/bin/env node
+
+// import minimist and express
+import minimist from "minimist"
+import express from 'express'
+
+const args = minimist(process.argv.slice(2))
+const port = args.port || 5000
+const app = express()
+
+app.use(express.urlencoded({extended: true}));
+
+// root endpoint
+app.get('/app/', (req, res) => {
+	res.status(200).send("200 OK")
+});
