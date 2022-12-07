19ddebcc60d355d3d262cf9a3ca3625e33d51b2b
corbinj47 <corbinj@email.unc.edu>
2022-12-07 16:18:52 -0500

    created interaction database and needed files

--- userDatabase.js
+++ userDatabase.js
@@ -3,3 +3,5 @@ import {database} from 'better-sqlite3';
 //create database connection
 const db = new Database('user.db');
 db.pragma('journal_mode = WAL');
+
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`);
